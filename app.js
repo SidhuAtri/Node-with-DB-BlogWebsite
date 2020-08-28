ca13926e227a4fb8630764f27c3cd40307b97fe5
sidharth atri <sidharthatri@gmail.com>
2020-08-28 10:06:28 +0530

    update

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const homeStartingContent =
   "Motivation is your desire to do something with your personal life, at work, in school, in sports, or in any hobbies. Having the motivation to do something can help you achieve your big goals and dreams, whatever they may be.Knowing how to motivate yourself can help you accomplish anything you set your mind to, so let’s get to that next.";
 const aboutContent =
   " I am well-organized, known for the thorough and methodical approach that I take when researching and planning. An excellent team-player, able to listen, negotiates and communicates persuasively and encouragingly with people. I am a Full Stack Web Developer & Programmer. To Know More Visit - https://sidharthatri.netlify.app/ ";
-const contactContent =
-  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const contactContent = "To be Updated Soon .....";
 
 const app = express();
 
@@ -68,6 +67,10 @@ app.get("/posts/:postId", function (req, res) {
 
 app.get("/about", function (req, res) {
   res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
 });
 
 app.listen(process.env.PORT || 3000, function () {
