c7bf563c1cdfb8ab0f2a36a5a384a017879bde44
sidharth atri <sidharthatri@gmail.com>
2020-08-28 10:04:45 +0530

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const homeStartingContent =
   "Motivation is your desire to do something with your personal life, at work, in school, in sports, or in any hobbies. Having the motivation to do something can help you achieve your big goals and dreams, whatever they may be.Knowing how to motivate yourself can help you accomplish anything you set your mind to, so let’s get to that next.";
 const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+  " I am well-organized, known for the thorough and methodical approach that I take when researching and planning. An excellent team-player, able to listen, negotiates and communicates persuasively and encouragingly with people. I am a Full Stack Web Developer & Programmer. To Know More Visit - https://sidharthatri.netlify.app/ ";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -68,10 +68,6 @@ app.get("/posts/:postId", function (req, res) {
 
 app.get("/about", function (req, res) {
   res.render("about", { aboutContent: aboutContent });
-});
-
-app.get("/contact", function (req, res) {
-  res.render("contact", { contactContent: contactContent });
 });
 
 app.listen(process.env.PORT || 3000, function () {
